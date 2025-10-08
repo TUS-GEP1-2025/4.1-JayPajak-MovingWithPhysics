2fa184ebd7beffcc93278891e98c5472a90ddb7e
Jagoda-Pajak-TUS24 <K00320720@student.tus.ie>
2025-10-08 15:24:44 +0100

    added moevemnt

--- Assets/movingPhysics.cs
+++ Assets/movingPhysics.cs
@@ -2,18 +2,34 @@ using JetBrains.Annotations;
 using UnityEngine;
 
 public class movingPhysics : MonoBehaviour
+
 {
+    public Rigidbody2D theRB;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     { 
-        PublicAPIAttribute Rigidbody2D theRB;
         Rigidbody2D Steve = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 vectorRight;
-        vectorRight = Vector2.right * 5;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            theRB.linearVelocity = Vector2.right * 5;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            theRB.linearVelocity = Vector2.left * 5;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            theRB.AddForce(Vector2.up * 20);
+        }
+
+
     }
 }
