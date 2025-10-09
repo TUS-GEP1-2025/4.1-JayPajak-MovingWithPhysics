48315ec803fc906485c0efea01f83377f5e08476
Jagoda-Pajak-TUS24 <K00320720@student.tus.ie>
2025-10-09 11:57:41 +0100

    added colliders, audio

--- Assets/movingPhysics.cs
+++ Assets/movingPhysics.cs
@@ -9,10 +9,16 @@ public class movingPhysics : MonoBehaviour
     public float MovementSpeed = 5.0f;
     public float JumpForce = 200.0f;
 
+    public bool isGrounded = false;
+
+    public AudioSource theAudioSource;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    { 
+    {
         //Rigidbody2D Steve = gameObject.GetComponent<Rigidbody2D>();
+        GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,17 +35,41 @@ public class movingPhysics : MonoBehaviour
             theRB.linearVelocity = Vector2.right * MovementSpeed;
         }
 
-        else if (Input.GetKey(KeyCode.LeftArrow) == true)
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
         {
             theRB.linearVelocity = Vector2.left * MovementSpeed;
         }
+
     }
 
     public void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) == true)
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.UpArrow) == true)
         {
             theRB.AddForce(Vector2.up * 200);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "GroundPlatform")
+        {
+            isGrounded = true;
+        }
+
+        if (collision.gameObject.tag == "Wall")
+        {
+            theAudioSource.Play();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "GroundPlatform")
+        {
+            isGrounded = false;
+        }
+    }
+
+
 }
